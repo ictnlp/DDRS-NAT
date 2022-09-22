d647b4756ec684e5159a78e338b44bbe044e508b
shaochenze <42638348+shaochenze@users.noreply.github.com>
2022-09-22 17:18:44 +0800

    Implement NMLA

--- fairseq/criterions/nat_loss.py
+++ fairseq/criterions/nat_loss.py
@@ -11,6 +11,7 @@ from fairseq import metrics, utils
 from fairseq.criterions import FairseqCriterion, register_criterion
 from torch import Tensor
 from nltk.translate.bleu_score import sentence_bleu
+from collections import Counter
 
 @register_criterion("nat_loss")
 class LabelSmoothedDualImitationCriterion(FairseqCriterion):
@@ -35,6 +36,9 @@ class LabelSmoothedDualImitationCriterion(FairseqCriterion):
             default=0)
         parser.add_argument(
             '--tune',
+            action="store_true")
+        parser.add_argument(
+            '--use-ngram',
             action="store_true")
 
     def _compute_loss(
@@ -295,8 +299,11 @@ class DDRSCriterion(LabelSmoothedDualImitationCriterion):
         if not self.args.tune:
             loss = self.curr_loss(log_probs, div_tgt_tokens, div_tgt_lengths, 
                 ctc_input_lengths, step)
-        else:
-            loss = self.rl_loss(log_probs, div_tgt_tokens, ctc_input_lengths, model.tgt_dict)
+        elsei:
+            if self.args.use_ngram:
+                loss = self.gram_loss(log_probs, div_tgt_tokens, ctc_input_lengths, model.tgt_dict)
+            else:
+                loss = self.rl_loss(log_probs, div_tgt_tokens, ctc_input_lengths, model.tgt_dict)
 
         sample_size = 1
         logging_output = {
@@ -308,6 +315,67 @@ class DDRSCriterion(LabelSmoothedDualImitationCriterion):
         }
         return loss, sample_size, logging_output
 
+    def gram_loss(self, log_probs, div_tgt_tokens, ctc_input_lengths, tgt_dict):
+
+        log_probs = log_probs.transpose(0,1)
+        batch_size, length_ctc, vocab_size = log_probs.size()
+        probs = torch.exp(log_probs)
+        num_refs = len(div_tgt_tokens)
+        for i in range(num_refs):
+            div_tgt_tokens[i] = div_tgt_tokens[i].tolist()
+        probs_blank = probs[:,:,4]
+        length = probs[:,1:,:] * (1 - probs[:,:-1,:])
+        length[:,:,4] = 0
+        expected_length = torch.sum(length).div(batch_size)
+
+        logprobs_blank = log_probs[:,:,4]
+        cumsum_blank = torch.cumsum(logprobs_blank, dim = -1)
+        cumsum_blank_A = cumsum_blank.view(batch_size, 1, length_ctc).expand(-1, length_ctc, -1)
+        cumsum_blank_B = cumsum_blank.view(batch_size, length_ctc, 1).expand(-1, -1, length_ctc)
+        cumsum_blank_sub = cumsum_blank_A - cumsum_blank_B
+        cumsum_blank_sub = torch.cat((torch.zeros(batch_size, length_ctc,1).cuda(cumsum_blank_sub.get_device()), cumsum_blank_sub[:,:,:-1]), dim = -1)
+        tri_mask = torch.tril(utils.fill_with_neg_inf(torch.zeros([batch_size, length_ctc, length_ctc]).cuda(cumsum_blank_sub.get_device())), 0)
+        cumsum_blank_sub = cumsum_blank_sub + tri_mask
+        blank_matrix = torch.exp(cumsum_blank_sub)
+
+        gram_1 = []
+        gram_2 = []
+        gram_count = []
+        rep_gram_pos = []
+        num_grams = 0
+        for i in range(batch_size):
+            two_grams = Counter()
+            gram_1.append([])
+            gram_2.append([])
+            gram_count.append([])
+            for j in range(num_refs):
+                curr_tgt = div_tgt_tokens[j][i]
+                for k in range(len(curr_tgt) - 1):
+                    if curr_tgt[k+1] != 1:
+                        two_grams[(curr_tgt[k], curr_tgt[k+1])] += 1/num_refs
+            for two_gram in two_grams:
+                gram_1[-1].append(two_gram[0])
+                gram_2[-1].append(two_gram[1])
+                gram_count[-1].append(two_grams[two_gram])
+            num_grams = max(len(gram_count[-1]), num_grams)
+        
+        for i in range(batch_size):
+            while len(gram_count[i]) < num_grams:
+                gram_1[i].append(1)
+                gram_2[i].append(1)
+                gram_count[i].append(0)
+        gram_1 = torch.LongTensor(gram_1).cuda(blank_matrix.get_device())
+        gram_2 = torch.LongTensor(gram_2).cuda(blank_matrix.get_device())
+        gram_count = torch.Tensor(gram_count).cuda(blank_matrix.get_device()).view(batch_size, num_grams,1)
+        gram_1_probs = torch.gather(probs, -1, gram_1.view(batch_size,1,num_grams).expand(batch_size,length_ctc,num_grams)).transpose(1,2).view(batch_size, num_grams, length_ctc, 1)
+        gram_2_probs = torch.gather(probs, -1, gram_2.view(batch_size,1,num_grams).expand(batch_size,length_ctc,num_grams)).transpose(1,2).view(batch_size, num_grams, 1, length_ctc)
+        probs_matrix = torch.matmul(gram_1_probs, gram_2_probs)
+        bag_grams = blank_matrix.view(batch_size, 1, length_ctc, length_ctc) * probs_matrix
+        bag_grams = torch.sum(bag_grams.view(batch_size, num_grams, -1), dim = -1).view(batch_size, num_grams,1)
+        match_gram = torch.min(torch.cat([bag_grams,gram_count],dim = -1), dim = -1)[0]
+        match_gram = torch.sum(match_gram).div(batch_size)
+        loss = (- 2 * match_gram).div(torch.sum(gram_count).div(batch_size) + expected_length - 1)
+        return loss
 
     def curr_loss(self, log_probs, div_tgt_tokens, div_tgt_lengths, ctc_input_lengths, step):
         num_references = len(div_tgt_tokens)
